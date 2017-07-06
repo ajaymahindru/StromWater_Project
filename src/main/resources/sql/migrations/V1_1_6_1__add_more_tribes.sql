682e8f22afa16f5010aba68b41e98568cfc5b3f5
dfladung <david.fladung@cgifederal.com>
2017-07-06 17:42:36 -0400

    Removing duplicate of Tejon Indian Tribe in BIA ref.

--- src/main/resources/sql/migrations/V1_1_6_1__add_more_tribes.sql
+++ src/main/resources/sql/migrations/V1_1_6_1__add_more_tribes.sql
@@ -4,4 +4,3 @@ insert into cgp_ref_bia_tribes(id, state_code, tribe_name) values(577,'CA','Coyo
 
 insert into cgp_ref_tribes(id, tribal_code, tribal_name) values(754,'-994','TEJON INDIAN TRIBE');
 insert into cgp_ref_tribe_states (tribal_code, state_code) values ('-994', 'CA');
-insert into cgp_ref_bia_tribes(id, state_code, tribe_name) values(578,'CA','Tejon Indian Tribe');
