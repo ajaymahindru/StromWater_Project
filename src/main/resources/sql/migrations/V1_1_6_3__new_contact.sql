e6df03ad3acd23662b7f6ac732e1a195835f578d
Linera Abieva <linera.abieva@cgifederal.com>
2017-09-05 12:11:48 -0400

    flyway script to add a new notification contact

---
+++ src/main/resources/sql/migrations/V1_1_6_3__new_contact.sql
@@ -0,0 +1,3 @@
+-- adding new subscriber (Idaho) per OES-870
+insert into cgp_ref_subscribers(id, subscriber_email, first_name, last_name, description)
+values(46,'sharonbarnes@fs.fed.us','Sharon', 'Barnes', 'FS contact');
