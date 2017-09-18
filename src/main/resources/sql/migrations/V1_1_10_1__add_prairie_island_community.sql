5bef75cc608303a4b3ef71bb09bdfd5ff76cf8f9
Fladung, David V (CGI Federal) <david.fladung@cgifederal.com>
2017-09-18 14:05:09 -0400

    Adding Prairie Island Community to Minnesota.

---
+++ src/main/resources/sql/migrations/V1_1_10_1__add_prairie_island_community.sql
@@ -0,0 +1,2 @@
+insert into cgp_ref_tribes(id, tribal_code, tribal_name) values(755,'-993','PRAIRIE ISLAND COMMUNITY');
+insert into cgp_ref_tribe_states (tribal_code, state_code) values ('-993', 'MN');
